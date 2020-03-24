372ed557c5620dbe846978eaf7e4d624dcf13778
Alex Krechet <puretrain@dream.com>
2020-03-24 22:25:25 +0200

    Query sample through custom func were added

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   ;:main clojure-train.core
-  :repl-options {:init-ns clojure-train.protocols-tutorial}
+  :repl-options {:init-ns clojure-train.data-structures.practical-example}
   )
---
+++ src/clojure_train/data_structures/practical_example.clj
@@ -0,0 +1,26 @@
+(ns clojure-train.data-structures.practical-example)
+
+(def orders
+  [{:product "Clock", :customer "Wile Coyote", :qty 6, :total 300}
+   {:product "Dynamite", :customer "Wile Coyote", :qty 20, :total 5000}
+   {:product "Shotgun", :customer "Elmer Fudd", :qty 2, :total 800}
+   {:product "Shells", :customer "Elmer Fudd", :qty 4, :total 100}
+   {:product "Hole", :customer "Wile Coyote", :qty 1, :total 1000}
+   {:product "Anvil", :customer "Elmer Fudd", :qty 2, :total 300}
+   {:product "Anvil", :customer "Wile Coyote", :qty 6, :total 900}
+   ])
+
+;;custom reduce-by function, receiving 4 args
+(defn reduce-by
+  [key-fn f init coll]
+  (reduce (fn [summaries x]
+            (let [k (key-fn x)]
+              (assoc summaries k (f (summaries k init) x))))
+          {} coll))
+
+;;total price of orders for every customer
+(reduce-by :customer #(+ %1 (:total %2)) 0 orders)
+
+;;list of customers for each product
+(reduce-by :product #(conj %1 (:customer %2)) #{} orders)
+
